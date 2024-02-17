353459935d2689509f8cf24745b15b331f5e22b4
Asish Nath <asish11nath@gmail.com>
2024-02-17 19:49:10 +0530

    Setup the react app and config tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl underline bg-purple-500'>Hello world!</h1>
+      <button className='btn'>Click</button>
+    </div>
+  );
+}
+
+export default App;
