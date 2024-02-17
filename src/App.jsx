32afe6b9852a043d4690020132f293b39ea15a05
Asish Nath <asish11nath@gmail.com>
2024-02-17 21:11:13 +0530

    Create a Navbar skeloton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl underline bg-purple-500'>Hello world!</h1>
-      <button className='btn'>Click</button>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main>content</main>
+      </div>
+    </Router>
   );
 }
 
